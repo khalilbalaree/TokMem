72183b6cabbe099282db361fa1f4083e6b9f2b33
Zijun <zijuncanada@gmail.com>
2025-09-20 11:31:22 +0800

    Update test_tokmem.sh

--- atomic/test_tokmem.sh
+++ atomic/test_tokmem.sh
@@ -6,9 +6,9 @@ python main_in_domain.py \
     --val_size 10 \
     --test_size 50 \
     --model_name "meta-llama/Llama-3.2-3B-Instruct" \
-    --num_epochs 3 \
-    --batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --num_epochs 1 \
+    --batch_size 2 \
+    --gradient_accumulation_steps 2 \
     --max_length 1280 \
     --max_instruction_tokens 1024 \
     --eval_batch_size 32 \
